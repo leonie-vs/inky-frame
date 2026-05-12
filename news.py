1aadb0081ac68c2ef0c5ecd58bb51c7ea94d1752
Leonie von Schirach <leonie.schirach@web.de>
2026-05-12 17:28:38 +0100

    feat: add code to display news

--- news.py
+++ news.py
@@ -31,3 +31,20 @@ for item in items[:5]:
     title = item.find("title").text
     headlines.append(title)
 
+# display
+graphics = PicoGraphics(DISPLAY)
+
+graphics.set_pen(inky_frame.WHITE)
+graphics.clear()
+
+graphics.set_pen(inky_frame.BLACK)
+
+graphics.text("BBC News", 20, 20, 600, 4)
+
+y = 70
+for h in headlines:
+    graphics.text("- " + h, 20, y, 600, 2)
+    y += 40
+
+graphics.update()
+
